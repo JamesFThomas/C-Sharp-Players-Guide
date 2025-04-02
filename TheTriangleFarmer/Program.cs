d3c2bf6a5fb72a5ad278f199120ad8eaef1f3cf4
James Thomas <jamesfeltonthomas@gmail.com>
2025-04-02 16:42:53 -0500

    Created program to compute area of a traingle from user inputs

---
+++ TheTriangleFarmer/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+    
+    Title: The Triangle Farmer 
+
+    Objectives: 
+
+        - Write a program that lets you input the traingle's base size & height
+
+        - Compute the srea of a traingle by truning the below equation into code
+            -- Area = base * height / 2
+
+        - Write the result of the coputation
+  
+*/
+
+
+string baseSize, height;
+
+Console.WriteLine("Enter the traingle's base size in inches.");
+
+baseSize = Console.ReadLine() ?? "";
+
+Console.WriteLine("Enter the traingle's height in inches.");
+
+height = Console.ReadLine() ?? "";
+
+int area = ( Int32.Parse(baseSize) * Int32.Parse(height)) / 2;
+
+Console.WriteLine($" The area of your triangle is {area} sqaure inches ");
