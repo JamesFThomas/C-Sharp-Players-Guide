4465533db747db2d21909dbce9480069f12e0284
James Thomas <jamesfeltonthomas@gmail.com>
2025-04-03 12:44:22 -0500

    Level 7 challeneges: 4 sisters & kings dominion

---
+++ TheDominionOfKings/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+    Title: The Dominion of Kings
+
+    Objectives: 
+        - Create a program that allows users to enter how many provinces, duchies, and estates they have. 
+
+        - Add up the user's total score, giving 1 point per estate, 3 per duchy, and 6 per porvince.
+
+        - Display the point totat to the user. 
+ 
+*/
+
+
+// Create variables for each string input provinces, duchies, estates;
+int provinces, duchies, estates, totalPoints;
+
+// Assign each variable to user input for corresponding question 
+
+Console.WriteLine("Enter the number of provinces you control.");
+
+provinces = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter the number of duchies you control.");
+
+duchies = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter the number of estates you control.");
+
+estates = Int32.Parse(Console.ReadLine());
+
+// create variable to hold total point value 
+
+
+// perform computation for total points 
+totalPoints = (provinces * 6) + (duchies * 3) + (estates * 1);
+
+
+// return total points 
+Console.WriteLine($"Your total points are {totalPoints}.");
+
---
+++ TheFourSistersAndTheDuckBear/Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+    Title: The Four Sisters and the Duckbear
+
+    Objectives:
+    
+    - Create a program that lets the user enter the number of choclate eggs gathered that day. 
+
+    - using the "/" division and "%" remainder oporators, compute how any eggs each sister should get and how many are left over for the duckbear.
+
+    - Question?
+        => What are the three total egg counts where the duckbear gets more than each sister does? 
+            -- use the prgram you created to help you find the answer.
+ 
+ */
+
+
+// create a variable that represents the number of sisters 
+int sisters = 4;
+
+// create a variable to hold the total--ount of eggs for the day
+int eggCount;
+
+Console.WriteLine("How many chocolate eggs did the chickens lay today");
+
+eggCount = Convert.ToInt32(Console.ReadLine());
+
+
+// create a variable that will hold the computation result 
+
+int eggsForSisters, eggsForDuckbear;
+
+
+eggsForSisters = eggCount / sisters;
+
+eggsForDuckbear = eggCount % sisters;
+
+Console.WriteLine($"Today each sister get {eggsForSisters} eggs while the duckbear gets {eggsForSisters} eggs.");
+
+
+// What are the three total egg counts where the duckbear gets more than each sister does? 
+    // answer => 
