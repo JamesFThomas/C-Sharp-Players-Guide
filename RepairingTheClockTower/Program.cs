206d8cd0d7087ee307df17276b3a54fa41092f97
James Thomas <jamesfeltonthomas@gmail.com>
2025-04-04 09:45:38 -0500

    level 9 challenges: Repairing the clocktower && watchtower

---
+++ RepairingTheClockTower/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+    Title: Repairing the Clocktower
+
+    Objectives:
+        - Take numbers a input from the console.
+
+        - Display the string "Tick" is even and "Tock" if the number is odd
+
+        - Hint: Remember to use the "%" modulus or remainder operator to determine if a number is even or odd.
+
+*/
+
+// input variable 
+int input;
+
+// pormpt user for input
+Console.Write("Enter a number: ");
+
+// Get/convert input from user 
+input = Convert.ToInt32(Console.ReadLine());
+
+// check if the number is even or odd
+if ( input % 2 == 0)
+{
+    Console.Write("Tick");
+}
+else 
+{
+    Console.Write("Tock");
+}
+
---
+++ Watchtower/Program.cs
@@ -0,0 +1,85 @@
+﻿/*
+ 
+    Title: Watchtower 
+
+    Cordinate Diagram:       
+
+                    x < 0      x = 0      x > 0
+                   +-------+-------+-------+
+            y > 0  |  NW   |   N   |  NE   |
+                   +-------+-------+-------+
+            y = 0  |   W   |   !   |   E   |
+                   +-------+-------+-------+
+            y < 0  |  SW   |   S   |  SE   |
+                   +-------+-------+-------+
+
+    Objectives:
+
+    - Ask the user for an x value and a y value. These are the cordinates of the enemy reltive to the watch tower's location
+
+    - Using the image on the right, if statements, and relational operators, display a message about what direction the enemy is coming from.   
+        -- ex: "The enemy is to the northwest!" || "The enemy is here"
+ 
+ */
+
+
+// create conditions for return messages 
+
+int x, y;
+
+Console.Title = "Watchtower";
+
+Console.Write("Enter the x coordinate of the enemy location: ");
+x = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter the y coordinate of the enemy location: ");
+y = Convert.ToInt32(Console.ReadLine());
+
+
+if ( x < 0 && y > 0)
+{
+    Console.WriteLine("The enemy is to the Northwest!");
+
+}
+
+if (x == 0 && y > 0)
+{
+    Console.WriteLine("The enemy is to the North!");
+}
+
+if (x > 0 && y > 0)
+{
+    Console.WriteLine("The enemy is to the Northeast!");
+}
+
+if (x < 0 && y == 0)
+{
+    Console.WriteLine("The enemy is to the West!");
+}
+
+if (x == 0 && y == 0)
+{
+    Console.WriteLine("The enemy is Here!");
+}
+
+if (x > 0 && y == 0 )
+{
+    Console.WriteLine("The enemy is to the East!");
+}
+
+if (x < 0 && y < 0)
+{
+    Console.WriteLine("The enemy is to the Southwest!");
+}
+
+if (x == 0 && y < 0)
+{
+    Console.WriteLine("The enemy is to the South!");
+}
+
+if (x > 0 && y < 0)
+{
+
+    Console.WriteLine("The enemy is to the Southeast!");
+}
+
