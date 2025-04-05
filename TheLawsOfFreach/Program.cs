6e84ab94bd831121277cc2d1cb1c8fcd2ad4db9d
James Thomas <jamesfeltonthomas@gmail.com>
2025-04-05 11:32:37 -0500

    Level 12  Arrays -> exercises

---
+++ TheLawsOfFreach/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+
+Title: The Laws of Freach 
+
+Objectives:
+   - Create an application that calculates the minimvalue and average of a array of intergers.
+
+   - Make sure your code uses a foreach loop instead of a for loop
+
+*/
+
+
+int[] array = new int[] { 3, 5, -7, -99, 32, 27, 45, 47 };
+
+int currentSmallest = int.MaxValue;
+int total = 0;
+
+foreach ( int number in array)
+{
+    if ( currentSmallest < number)
+    {
+        currentSmallest = number;
+    }
+    total += number;
+}
+
+int average = total / array.Length;
+
+Console.WriteLine($"The smallest number in the areaa is {currentSmallest}");
+Console.WriteLine($"The average of the array is {average}");
---
+++ TheReplicatorOfDTo/Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+ 
+Title: The Replicator Of D'To
+
+Objectives:
+    _ Make a program that contains creates an array of length 5.
+
+    _ Ask for the user to enter 5 numbers.
+
+    _ Make a second array with a length 5.
+
+    - Use a loop to copy the values out of the original array and into the new one.
+
+    - Print out the contents of each array one after the other to show the array was copied corretly.  
+ */
+
+string numbers;
+
+int[] copy = new int[5];
+
+Console.WriteLine("Please enter 5 numbers separated by a space: ");
+
+numbers = Console.ReadLine() ?? "";
+
+string[] numbersArray = numbers.Split(' ');
+
+for ( int x = 0; x < numbersArray.Length; x++ )
+{
+    // Convert each value of the string array to an int
+    // load the array using the index of the loop
+    copy[x] = Convert.ToInt32(numbersArray[x]);
+
+    // Print out the contents of the array
+    Console.WriteLine($"The value of the original array is: {numbersArray[x]}");
+    Console.WriteLine($"The value of the copied array is: {copy[x]}");
+
+}
+
+
+
+
+
+
+
