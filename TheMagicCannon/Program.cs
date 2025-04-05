7d79048208700ec8d2d80cae53ad031c7fdff288
James Thomas <jamesfeltonthomas@gmail.com>
2025-04-05 11:01:01 -0500

    Level 11 looping -> exercises;

---
+++ TheMagicCannon/Program.cs
@@ -0,0 +1,46 @@
+﻿/*
+ 
+ Title: The Magic Cannon 
+
+ Details:
+    - The new cannon crew has a new cannon that blasts out different types of energy.
+    - every third turn the cannon blasts out fire
+    - every fifth turn the cannon blasts out eltricity
+    - when those numbers line up the cannon produces a combined blast
+
+ Objective: 
+
+    - Write a program that will loop through the value between 1 and 100 and display what kind of blast the new cannon crew should expect.
+
+    - Change the color of the output text based on the type of blast:
+        - If the number is divisible by 3, display "Yoga Flame!" in red.
+        - If the number is divisible by 5, display "Hadoken!" in yellow.
+        - If the number is divisible by both 3 and 5, display "Combo Strike, Hadoken Flame!" in blue.
+    -
+ 
+ */
+
+for ( int x = 0; x <= 100; x++)
+{
+    if (x % 15 == 0)
+        {
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine("Combo Strike, Hadoken Flame!");
+    }
+    else if (x % 3 == 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Yoga Flame!");
+    }
+    else if (x % 5 == 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine("Hadoken!");
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(x);
+    }
+
+}
---
+++ ThePrototype/Program.cs
@@ -0,0 +1,55 @@
+﻿/*
+ Title: The Prototype
+
+ Objectives:
+    - Build a porgram that will allow a user, the pilot to enter a number. 
+
+    - If the numeber is above 100 orl less than 0, keep asking. 
+
+    - Clear the screen once the program has collected a good number.
+
+    - Ask a second user, the hunter, to guess numbers.
+
+    - Indicate wheather the user guessed too high, too low, or guessed right. 
+
+    - Loop untiil they get it right, then end the program.  
+*/
+
+Console.Title = "The Prototype";
+
+int user1Input, user2Input;
+
+Console.Write("Pilot, enter a number between 0 and 100: ");
+
+user1Input = Convert.ToInt32(Console.ReadLine());
+
+while ( user1Input < 0 || user1Input  > 100 )
+{
+    Console.Write("Bad choice, Pilot pick a new a number between 0 and 100 please: ");
+    user1Input = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.Clear();
+
+Console.Write("Hunter, guess the number the pilot just entered: ");
+
+user2Input = Convert.ToInt32(Console.ReadLine());
+
+while ( user2Input != user1Input)
+{
+    
+    if (user2Input < user1Input)
+    {
+        Console.WriteLine("Your guess is too low.");
+        user2Input = Convert.ToInt32(Console.ReadLine());
+
+    }
+    else if (user2Input > user1Input)
+    {
+        Console.WriteLine("Your guess is too high.");
+        user2Input = Convert.ToInt32(Console.ReadLine());
+    }
+    
+}
+
+Console.WriteLine("You guessed it right, Hunter! The number is: " + user1Input);
