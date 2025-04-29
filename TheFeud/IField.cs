62e9f5b7e07d3cc12a933fe0cac6e36762e5f07c
James Thomas <jamesfeltonthomas@gmail.com>
2025-04-29 10:48:02 -0500

    The Feud: - namespaces split into spearate files - working

---
+++ TheFeud/IField.cs
@@ -0,0 +1,20 @@
+﻿namespace TheFeud.IField
+{
+
+    public class Sheep
+    {
+        public void Sound()
+        {
+            Console.WriteLine("Baa");
+        }
+    }
+
+    public class Pig
+    {
+        public void Sound()
+        {
+            Console.WriteLine("Oink");
+        }
+    }
+
+}
---
+++ TheFeud/McDroid.cs
@@ -0,0 +1,20 @@
+﻿namespace TheFeud.McDroid
+{
+
+    public class Cow
+    {
+        public void Sound()
+        {
+            Console.WriteLine("Moo");
+        }
+    }
+
+    public class Pig
+    {
+        public void Sound()
+        {
+            Console.WriteLine("Oink");
+        }
+    }
+
+}
