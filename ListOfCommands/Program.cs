73ae9542ef8613ad6a1a1b9e170aabafe5901eef
James Thomas <jamesfeltonthomas@gmail.com>
2025-04-29 08:13:30 -0500

    List Of Commands - working with List and stop command updates

--- ListOfCommands/Program.cs
+++ ListOfCommands/Program.cs
@@ -18,3 +18,199 @@ Objectives:
  
 
 */
+
+
+
+
+RobotProgram robotProgram = new RobotProgram();
+
+robotProgram.RunProgram();
+
+
+public class RobotProgram
+{
+    bool isCollectingCommands = true;
+    public void RunProgram()
+    {
+        // Create a new Robot instance
+        Robot robot = new Robot();
+
+        // Collect commands until the user types "stop"
+        CollectCommands(robot);
+
+        // Run the commands
+        robot.Run();
+
+    }
+
+    private IRobotCommand? CreateCommand(string input)
+    {
+        IRobotCommand? command = null;
+
+        switch (input)
+        {
+            case "on":
+                command = new OnCommand();
+                break;
+            case "off":
+                command = new OffCommand();
+                break;
+            case "north":
+                command = new NorthCommand();
+                break;
+            case "south":
+                command = new SouthCommand();
+                break;
+            case "east":
+                command = new EastCommand();
+                break;
+            case "west":
+                command = new WestCommand();
+                break;
+        }
+
+        return command;
+    }
+
+    // Update the CollectCommands method to handle the nullable return value of CreateCommand
+    public void CollectCommands(Robot robot)
+    {
+        string? input;
+        string prompt = "Enter your robot command (on/off/north/south/east/west) or type 'stop' to finish:";
+        
+        while (isCollectingCommands)
+        {
+            Console.WriteLine(prompt);
+            input = Console.ReadLine();
+
+            if (input != null && input.ToLower() == "stop")
+            { 
+                isCollectingCommands = false;
+                break;                
+            }
+
+            if (input == null || !IsGoodInput(input))
+            {
+                Console.WriteLine("Invalid input. Please enter a valid command.");
+                CollectCommands(robot);
+                return;
+
+            }
+            
+            
+            else
+            {
+                IRobotCommand? command = CreateCommand(input);
+
+                if (command != null) 
+                {
+                    robot.Commands.Add(command);
+                }
+            }
+
+        }
+
+    }
+
+
+     public bool IsGoodInput(string input)
+    {
+        bool goodInput = false;
+
+        if (input.ToLower() == "on" || input.ToLower() == "off" || input.ToLower() == "north" || input.ToLower() == "south" || input.ToLower() == "east" || input.ToLower() == "west")
+        {
+            goodInput = true;
+        }
+
+        return goodInput;
+
+    }
+
+}
+
+// replaced RobotCommand abstract class with IRobotCommand interface
+public interface IRobotCommand
+{
+    public void Run(Robot robot);
+
+}
+
+public class OnCommand : IRobotCommand
+{
+    public void Run(Robot robot)
+    {
+        robot.IsPowered = true;
+    }
+}
+
+public class OffCommand : IRobotCommand
+{
+    public void Run(Robot robot)
+    {
+        robot.IsPowered = false;
+    }
+}
+
+public class NorthCommand : IRobotCommand
+{
+    public void Run(Robot robot)
+    {
+        if (robot.IsPowered)
+        {
+            robot.Y++;
+        }
+    }
+}
+
+
+public class SouthCommand : IRobotCommand
+{
+    public void Run(Robot robot)
+    {
+        if (robot.IsPowered)
+        {
+            robot.Y--;
+        }
+    }
+
+}
+
+public class EastCommand : IRobotCommand
+{
+    public void Run(Robot robot)
+    {
+        if (robot.IsPowered)
+        {
+            robot.X++;
+        }
+    }
+}
+
+public class WestCommand : IRobotCommand
+{
+    public void Run(Robot robot)
+    {
+        if (robot.IsPowered)
+        {
+            robot.X--;
+        }
+    }
+}
+
+public class Robot
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+    public bool IsPowered { get; set; }
+    public List<IRobotCommand> Commands { get; } = new List<IRobotCommand>();
+
+    public void Run()
+    {
+        foreach (IRobotCommand? command in Commands)
+        {
+            command?.Run(this);
+            Console.WriteLine($"[{X} {Y} {IsPowered}]");
+        }
+    }
+
+}
