27840cebc8a1471990e2a4c18d2b350d83115c8c
James Thomas <jamesfeltonthomas@gmail.com>
2025-04-30 11:02:15 -0500

    Levlel 34: methods Revisited - Safe Number Crunching - working - Better Random - outlined

---
+++ BetterRandom/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+ 
+Title: Better Random 
+
+
+Story:
+
+The Villagers of Randertherin often use the Random class but struggle with it's limited capabilities. 
+They have asked for your help to make Random better. They offer you the Medallion of Powerful Methods in exchange. 
+Their complaints are as follow: 
+
+- Random.NextDouble() only return values between 0 and 1, and they often need to be bale to produce random double values between 0 and another number, such as 0 to 10.
+
+- They need to randomly choose from one of several strings, such as "up", "down", "left", "right", with each having an equal probability of being chosen.
+
+- They need to do a coin toss randomly picking a bool, and usually want it to be a fair coin toss (50% heads and 50% tails) but occasionally want unequal probabilities. 
+    --> example: 75% chance of true and 25% chance of false
+
+Objective: 
+
+- Create a new static class to ad extensions methods for Random 
+
+- As described above, add, a NextDouble extension method that gives a maximum value for a randomly generated double.
+
+- Add a NextString extension method Random that allows you to pass in any number of string values ( using params ) and randomly pick one of them.
+
+- Add a CoinFlip method that randomly picks a bool value. It should have an optional parameter that indicates the frequency of heads coming up, which should default to .5, or 50% of the time.
+
+- Answer this question: In your opinion, would it be better to make a derived AdvancedRandom class that adds these methods or use extension methods and why? 
+    --> Answer: 
+ 
+ 
+*/
---
+++ SafeNumberCrunching/Program.cs
@@ -0,0 +1,127 @@
+﻿/*
+ 
+Title: Safe Number Crunching 
+
+
+Story:
+
+"Master Programmer we need you help!" We are but simple number crunchers. We need numbers in, work with them for a bit, then display the results. 
+But not everybody enters good numbers. Sometimes, we type in the wrong things by accident. And sometimes, somebody does it on purpose.
+Trolls, looking to cause trouble, I tell ya.
+
+We heard about these TryParse methods that can ot fail or break. We know you're here looking for Medallions and allies. 
+If you can help us with this, the Medallion of Reference Passing is yours, and we will join you at the final battle.
+
+
+Objective: 
+
+- Create a program that asks the user for an int value. Use the static int.TryParse(string s, out int result) method to parse the number. Loop until they enter a valid number.
+
+- Extend the program to do the same for both double and bool. 
+ 
+ 
+*/
+
+
+
+
+// create a method that loops collecting a int, double, and bool from the user
+// end the loop when the user enters a valid input for all types
+
+CollectUserValues();
+
+
+
+void CollectUserValues()
+{
+    Console.WriteLine("Welcome to the Safe Number Crunching program!");
+    int intValue = ValidIntInput();
+    double doubleValue = ValidDoubleInput();
+    bool boolValue = ValidBoolInput();
+    Console.WriteLine($"You entered: {intValue}, {doubleValue}, {boolValue}");
+    Console.WriteLine("Thank you for using the Safe Number Crunching program!");
+
+}
+
+int ValidIntInput()
+{
+    string prompt = "Please enter an integer value: ";
+    string blankPrompt = "You must enter a value: null values or empty/white space strings are not valid.";
+    string intPrompt = "You must enter a valid integer value: example 1, 2, 3, -1, -2 -3";
+
+    Console.Write(prompt);
+
+    string? input = Console.ReadLine();
+
+    if (String.IsNullOrWhiteSpace(input))
+    { 
+        Console.WriteLine(blankPrompt);
+        return ValidIntInput();
+    }
+
+    if ( !int.TryParse(input, out int result))
+    {
+        Console.WriteLine(intPrompt);
+        return ValidIntInput();
+    }
+
+    return result;
+}
+
+double ValidDoubleInput()
+{
+    string prompt = "Please enter a double value: ";
+    string blankPrompt = "You must enter a value: null values or empty/white space strings are not valid.";
+    string doublePrompt = "You must enter a valid double value: example 1.0";
+
+    Console.Write(prompt);
+    
+    string? input = Console.ReadLine();
+    
+    if (String.IsNullOrWhiteSpace(input))
+    {
+        Console.WriteLine(blankPrompt);
+        return ValidDoubleInput();
+    }
+    
+    bool wasConverted = double.TryParse(input, out double result);
+
+    if (!wasConverted)
+    {
+        Console.WriteLine(doublePrompt);
+        return ValidDoubleInput();
+    }
+
+    return result;
+}
+
+bool ValidBoolInput()
+{
+    string prompt = "Please enter a boolean value (true/false): ";
+    string blankPrompt = "You must enter a value: null values or empty/white space strings are not valid.";
+    string validPrompt = "You must enter a valid boolean value: example true or false";
+
+    Console.Write(prompt);
+
+    string? input = Console.ReadLine();
+
+    if (String.IsNullOrWhiteSpace(input))
+    {
+        Console.WriteLine(blankPrompt);
+        return ValidBoolInput();
+    }
+
+    if ( input.ToLower() != "true" && input.ToLower() != "false")
+    {
+        Console.WriteLine(validPrompt);
+        return ValidBoolInput();
+    }
+
+    if (!bool.TryParse(input, out bool result))
+    {
+        Console.WriteLine(validPrompt);
+        return ValidBoolInput();
+    }
+        return result;
+
+}
