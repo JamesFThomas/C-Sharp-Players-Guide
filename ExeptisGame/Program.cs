5b4e1270359fbe79851c2825e397c05d41a030a9
James Thomas <jamesfeltonthomas@gmail.com>
2025-05-01 08:59:47 -0500

    Level 35 Error Handling & Exceptions: - Exepti's Game - outlined

---
+++ ExeptisGame/Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+ 
+Title: Exepti's Game 
+
+
+
+Story:
+On the Island of Exceptions, you find the village of Exepti, which has seen little happiness since the arrival of the Uncoded One.
+The Exceptians you to have a game they played call cookie exceptions. 
+The village leader promises the warriors of Exepti will join you against the Uncoded One if you can recreate the ancient tradition in a program.   
+The village will give you the Medallion of Exceptions as well.
+
+Cookie exception is played by gathering nine chocolate chip cookies and one oatmeal raisin cookie. 
+The cookies are mixed and put in a dark room with two players who can't see the cookies. 
+Each player takes a turn picking a cookie randomly and shoving it into their mouths without seeing whether it is a delicious chocolate chip cookie or an awful oatmeal raisin.
+If they pick wrong and eat the oatmeal cookie they lose. 
+If their opponent eats the oatmeal cookie they win. 
+
+Objective: 
+
+- The game will pick a random number between 0 and 9.
+
+- The game will allow players to take turns picking numbers between 0 and 9.
+
+- If a player repeats a number that has already been used the program should make them select another.
+    --> Hint: If you use a List<T> to store previously chosen numbers, you can use its Contains method to see if a number has been used before.  
+
+- If the number matches the one the game picked initially, an exception should be thrown, terminating the program. 
+    --> Run the program at least once like this to see it crash. 
+
+- Put in a try catch block to handle the exception and display the result. 
+
+- Answer this question: Did you make a custom Exception type or use a existing one? Why did you chose to do what you did? 
+   =>
+
+- Answer this question: You could write this program without exceptions for learning purposes. If you didn't have that requirement, would you have used an exception? Why or why not?   
+    =>
+
+
+ 
+*/ 
