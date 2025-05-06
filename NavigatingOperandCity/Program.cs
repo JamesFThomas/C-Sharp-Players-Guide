b9f25833c0249ee30a84a8182573dae767d4ed90
James Thomas <jamesfeltonthomas@gmail.com>
2025-05-06 09:49:39 -0500

    Level 41 Operator Overloading: - Navigating Operand City - outlined
    Levle 42 Query Expressions
    - Three Lenses - outlined

---
+++ NavigatingOperandCity/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+
+Title: Navigating Operand City
+
+Story: 
+
+The city of operand is a carefully organized city organized into city blocks, lined up north to south and east to west.
+Blocks are referred to by their coordinates in the city, as we saw in the Cavern of Objects.
+The inhabitants of the town use the following three types as they work with city blocks:
+
+- public record BlockCoordinate(int Row, int Column);
+- public record BlockOffset(int RowOffset, int ColumnOffset);
+- public enumeration Direction { North, South, East, West }
+
+BlockCoordinate refers to to a specific blocks location, BlockOffset is for relative distances between blocks, and Direction specifies directions. 
+As we saw with the Cavern of Objects, rows start at 0 the North end of the city and get bigger as you go south, while columns start at 0 on the west end and get bigger as you go east.   
+The city has used these types for a long time, but the problem is that they don't play nice with each other.
+The town is the steward of three Medallions of code.
+They will give you each of them if you can use them to make life more manageable. 
+Use the code above as a starting point for what you would build. 
+
+In exchange for the medallion of Operators, they ask you to make it easy to add BlockCoordinate with a Direction and also with a BlockOffset to get a new BlockCoordinates.
+Add operators to BlockCoordinates to achieve this. 
+
+Objectives:
+
+- Use the code above as a starting point. 
+
+- Add an addition (+) to BlockCoordinates that takes a BlockCoordinate and a BlockOffset as arguments and produces a new BlockCoordinate that refers to the one you would arrive at by starting at the original coordinate and moving by the offset.
+That is if we started at (4,3) and had an offset of (2,0) we should end up at (6,3).
+
+- Add another addition (+) to BlockCoordinate that take a BlockCoordinate and a Direction as arguments and produces a new BlockCoordinate that is a block in the direction indicated if we started at (4,3) and went east, we should end up at (4,4).  
+
+- Write code to ensure that both operators are working correctly. 
+
+
+ 
+*/ 
---
+++ ThreeLenses/Program.cs
@@ -0,0 +1,47 @@
+﻿/*
+
+Title: The three Lenses
+
+Story: 
+
+The Guardian of the Medallion of Queries, Lennik has long awaited when he can return the medallion to a worthy programmer.    
+But he only wants to give to someone who truly understands the value of queries. 
+He requires you to build a solution to a simple problem three times over. 
+Lennik gives you the following array of positive numbers [1,9,2,8,3,7,4,6,5].
+He asks you to make a new collection form this data where: 
+
+- All the odd numbers are filtered out and only the even numbers should be considered
+
+- The numbers are in order
+
+- The numbers are doubled
+
+For example, with the array above:
+
+- filtered for evens should in [2,8,4,6]
+
+- The ordering step would result in [2,4,6,8]
+
+- The doubling step should result in [4,8,12,16]
+
+
+
+Objectives:
+
+- Write a method that will take int[] as an input and produce an IEnumerable<int> (it could be a list or array if you want) that meets all three of the conditions above using only procedural code - if statements, switches and loops.
+    --> Hint: the static Array.Sort method might be a useful tool here. 
+
+- Write a method that will take int[] as an input and produce an IEnumerable<int> that meets all three of the conditions above using keyword-based query expressions ( from x, where x, select x etc. ) 
+
+- Write a method that will take int[] as an input and produce an IEnumerable<int> that meets all three of the conditions above using method-call-based query expression ( x.Select(n => n + 1 ), x.where( n => n < 0), etc )
+
+- Run all three methods and display the results to ensure the produce the correct answers
+
+- Answer this question: Compare the size and understandability of these three approaches. Do any stand out as being particularly good or good to you? 
+    =>
+
+- Answer this question: Of these three approaches which is you personal favorite and why? 
+    =>
+
+ 
+*/ 
