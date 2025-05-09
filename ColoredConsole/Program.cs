382dbe530525f4ca7fa44fafdb6a6a81c05964db
James Thomas <jamesfeltonthomas@gmail.com>
2025-05-09 15:59:16 -0500

    Level 48 Beyond A Single Project: - Color Console - outlined

---
+++ ColoredConsole/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+
+Title: Colored Console
+
+
+Story: 
+
+The medallion of Large Solutions lies behind a sealed stone door and can only be unlocked by building a solution with two correctly linked projects. 
+this multi-project solution is the key that unseals the door. 
+
+
+Objectives:
+
+- Create a new console application from scratch.
+
+- Add a second Class Library project to the solution. 
+
+- Add the method public static string Prompt(string question) that writes question to the console window, then switches to cyan to get the user's response all on the same line.
+
+- Add the method public static void WriteLine(string text, ConsoleColor color) that writes the given text on its own line in the given color. 
+
+- Add the method public static void Write(string text, ConsoleColor color) that writes the given text without a new line in the given color.  
+
+- Add the right references between projects so that the main program can use the following code.  
+
+string name = ColorConsole.Prompt("What is your name")
+ColorConsole.WriteLine("Hello " + nae, ConsoleColor.Green);
+*/
