ae2fe0485a16fb382304995f69cc6eb31a68e9b0
James Thomas <jamesfeltonthomas@gmail.com>
2025-05-12 11:05:17 -0500

    Level 52: Finale Batlle - application entry point created

---
+++ TheFinalBattle/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+
+Title: The Final Battle
+
+Story: 
+
+The final battle has arrived. 
+
+On a volcanic island, enshrouded in a cloud of ash, you have reached the lair of the of the Uncoded One.
+
+You have prepared for this fight and you will return Programming to the lands. 
+
+Your allies have gathered to engage the Uncoded One's minions on the volcanic slopes while you ( and maybe a companion or two ) strike into the heart of the Uncoded One's lair to battle and destroy it. 
+
+Only a True Programmer will be able to survive the encounter, defeat the Uncoded One, and escape!
+ 
+*/
+
+namespace TheFinalBattle
+{
+    internal class Program
+    {
+
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("The Finale Battle is working!");
+        
+        }
+        
+
+    }
+}
